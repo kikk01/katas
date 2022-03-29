eb3c4a604ced85ac39b12e5d52c67f7cf56a0098
kikk01 <hugobar@live.fr>
2022-03-29 08:12:09 +0200

    restart and finish

--- moneyChange.php
+++ moneyChange.php
@@ -1,22 +1,34 @@
 <?php
 
+class Change {
+    public int $bill10 = 0;
+    public int $bill5 = 0;
+    public int $coin2 = 0;
 
-foreach(range(1, 15) as $i) {
-    var_dump($this->computeChangeDistribution($i));
-}
+    function calculChange(int $n) {
+        if ($n === 1 || $n === 5) {
+            var_dump(null);
+            return;
+        }
 
-function computeChangeDistribution(int $change)
-{
-    $reste = $change / 5; 
-    $p5 = floor($change / 5);
-    $reste = $change - ($p5*5);
-    if ($reste%2 === 0) {
-        $p2 = $reste / 2;
-        return [$p5, $p2];
-    } else {
-        $p5 = $p5 - 1;
-        $reste = $reste + 5;
-        $p2 = $reste / 2;
-        return [$p5, $p2];
+        if ($n%2 !== 0) {
+            $this->bill5 = 1;
+            $n = $n - 5;
+        }
+            
+        $this->bill10 = intval($n / 10);
+
+        $n = $n - ($this->bill10*10);
+        $this->coin2 = intval($n / 2);
+
+        var_dump($this->bill10);
+        var_dump($this->bill5);
+        var_dump($this->coin2);
+        
+        
     }
 }
+
+$change = new Change;
+
+$change->calculChange(8);
